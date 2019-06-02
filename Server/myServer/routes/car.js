0321f91bd7d6f684edb15729057b8e82efbfb759
William000000 <sugiraw@gmail.com>
2019-06-02 23:28:08 +0200

    [purchase-order-#166400666] api to perform purchase order

--- Server/myServer/routes/car.js
+++ Server/myServer/routes/car.js
@@ -1,4 +1,8 @@
 import express from "express";
+import carController from "../controllers/car";
 const router = express.Router();
+
+router.post("/car", carController.addCarPost);
+router.post("/order", carController.purchaseOrder);
 
 export default router;
