27d530e8b9c1eb28ce25bbba519fbe529465a43f
William000000 <sugiraw@gmail.com>
2019-06-02 20:26:51 +0200

    [signup#166399976] setting up server and signup api

---
+++ Server/myServer/modals/cars.js
@@ -0,0 +1,36 @@
+let cars = [
+    {
+        id: 1,
+        owner: 1,
+        created_on: new Date(),
+        state: "new",
+        status: "available",
+        price: 5000.0,
+        manufacturer: "lamborghini",
+        model: "lamborghini",
+        body_type: "car"
+    },
+    {
+        id: 2,
+        owner: 1,
+        created_on: new Date(),
+        state: "new",
+        status: "available",
+        price: 10000.0,
+        manufacturer: "Range",
+        model: "Range",
+        body_type: "car"
+    },
+    {
+        id: 3,
+        owner: 2,
+        created_on: new Date(),
+        state: "new",
+        status: "sold",
+        price: 12000.0,
+        manufacturer: "BMW",
+        model: "BMW",
+        body_type: "car"
+    }
+]
+export default cars;
---
+++ Server/myServer/modals/order.js
@@ -0,0 +1,26 @@
+let order = [
+{
+    id: 1,
+    buyer: 1,
+    car_id: 1,
+    amount: 10000,
+    status: "accepted"
+},
+{
+    id: 2,
+    buyer: 1,
+    car_id: 2,
+    amount: 10000,
+    status: "pending"
+},
+{
+    id: 3,
+    buyer: 1,
+    car_id: 2,
+    amount: 10000,
+    status: "accepted"
+}
+
+]
+
+export default order;
