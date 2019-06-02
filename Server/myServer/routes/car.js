e479842603811a2629317c5b34d47fa83bc2f4ed
William000000 <sugiraw@gmail.com>
2019-06-02 23:55:29 +0200

    [update-purchase-order-#166400735] api to update  purchase order

--- Server/myServer/routes/car.js
+++ Server/myServer/routes/car.js
@@ -3,8 +3,7 @@ import carController from "../controllers/car";
 const router = express.Router();
 
 router.post("/car", carController.addCarPost);
-
 router.post("/order", carController.purchaseOrder);
-
+router.patch("/order/:id/price",carController.updatePriceOfOrder);
 
 export default router;
