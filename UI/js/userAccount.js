5fe420c0230ed2a400943864a786d06806a7950e
William000000 <sugiraw@gmail.com>
2019-06-05 16:19:43 +0200

    [ch-reset-password-and-logout-and-desing-fix-#166470555]Add chore to reset password and logout btn and other page design fix

---
+++ UI/js/userAccount.js
@@ -0,0 +1,12 @@
+var userAccDiv = document.getElementById("userAcc");
+var userImg = document.getElementById("userImg");
+var space_hide = document.getElementById("wrap1");
+userImg.onmouseover = function() {
+  userAccDiv.style.display = "block";
+}
+
+space_hide.onmouseover = function() {
+  userAccDiv.style.display = "none";
+}
+
+
