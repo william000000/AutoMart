c7c71980d4200954625d00931cde52655de6fe2b
William000000 <sugiraw@gmail.com>
2019-06-18 12:15:15 +0200

    [#166750650 Fix error on Test-DB BUG] create table for testing

--- Server/db/table.js
+++ Server/db/table.js
@@ -5,15 +5,15 @@ import { loadavg } from "os";
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.NODE_ENV==="TEST"?process.env.TS_DATABASE_URL:process.env.DATABASE_URL,
+    connectionString: (process.env.NODE_ENV==="TEST")?process.env.TS_DATABASE_URL:process.env.DATABASE_URL,
 });
 
 const dropTable = [
-    `DROP TABLE IF EXISTS orders `,
+    `DROP TABLE IF EXISTS orders`,
     `DROP TABLE IF EXISTS flags `,
     `DROP TABLE IF EXISTS tokens `,
-    `DROP TABLE IF EXISTS cars `,
-    `DROP TABLE IF EXISTS users `,
+    `DROP TABLE IF EXISTS cars cascade`,
+    `DROP TABLE IF EXISTS users cascade`,
 ];
 
 const createTables = async () => {
@@ -67,36 +67,35 @@ const createTables = async () => {
     )`;
 
     const dummy = [    
-    `insert into users(id,email,first_name,last_name,password,address,createdon,isAdmin) values (1,'willy@gmail.com','Musinga','Willy','$2b$10$2X70DV68hpCzB7.4dWt2VuvD.kqEKjNhwybv8YiukoUbF4vGfTYyC', 'Kigali','10/09/2000'true')`,
-    `insert into users(id,email,first_name,last_name,password,address,createdon,isAdmin) values (2,'bobo@gmail.com','Bobo','Willo','$2b$10$2X70DV68hpCzB7.4dWt2VuvD.kqEKjNhwybv8YiukoUbF4vGfTYyC', 'Nigeria','10/09/2000',false')`,
+    `insert into users(id,email,first_name,last_name,password,address,createdon,isAdmin) values (1,'willy@gmail.com','Musinga','Willy','$2b$10$2X70DV68hpCzB7.4dWt2VuvD.kqEKjNhwybv8YiukoUbF4vGfTYyC', 'Kigali','10/09/2000','true')`,
+    `insert into users(id,email,first_name,last_name,password,address,createdon,isAdmin) values (2,'bobo@gmail.com','Bobo','Willo','$2b$10$2X70DV68hpCzB7.4dWt2VuvD.kqEKjNhwybv8YiukoUbF4vGfTYyC', 'Nigeria','10/09/2000','false')`,
 
     `INSERT INTO cars(id,owner, createdon, state, status, price, manufacturer, model, image, body_type, carName)
-     VALUES(1,willy@gmail.com,'10/04/2019','new','sold',5000.0,'lamborghini','lamborghini','https://william000000.github.io/AutoMart/UI/img/7.jpg',car','lamborghini')`,
+     VALUES(1,'willy@gmail.com','10/04/2019','new','sold',5000.0,'lamborghini','lamborghini','https://william000000.github.io/AutoMart/UI/img/7.jpg','car','lamborghini')`,
 
      `INSERT INTO cars(id,owner, createdon, state, status, price, manufacturer, model, image, body_type, carName)
-     VALUES(2,willy@gmail.com,'12/04/2019','new','available',10000.0,'range','range','https://william000000.github.io/AutoMart/UI/img/7.jpg','car','range')`,
+     VALUES(2,'willy@gmail.com','12/04/2019','new','available',10000.0,'range','range','https://william000000.github.io/AutoMart/UI/img/7.jpg','car','range')`,
 
      `INSERT INTO cars(id,owner, createdon, state,  status, price, manufacturer, model, image, body_type, carName)
-     VALUES(3,willy@gmail.com,'12/04/2019','used','sold',12000.0,'bmw','https://william000000.github.io/AutoMart/UI/img/7.jpg','bmw','car')`,
+     VALUES(3,'willy@gmail.com','12/04/2019','used','sold',12000.0,'bmw','bmw','https://william000000.github.io/AutoMart/UI/img/7.jpg','bmw','car')`,
 
      `INSERT INTO cars(id,owner, createdon, state,  status,price, manufacturer, model, image, body_type, carName)
-     VALUES(4,willy@gmail.com,'12/04/2019','used','available',20000.0,'nissan','nissan','https://william000000.github.io/AutoMart/UI/img/7.jpg','truck','nissan')`,
+     VALUES(4,'willy@gmail.com','12/04/2019','used','available',20000.0,'nissan','nissan','https://william000000.github.io/AutoMart/UI/img/7.jpg','truck','nissan')`,
 
      `INSERT INTO cars(id,owner, createdon, state, status, price, manufacturer, model, image, body_type, carName)
-     VALUES(5,willy@gmail.com,'12/04/2019','used','available',20000.0,'fusso','fusso','https://william000000.github.io/AutoMart/UI/img/7.jpg','truck','fusso')`,
+     VALUES(5,'willy@gmail.com','12/04/2019','used','available',20000.0,'fusso','fusso','https://william000000.github.io/AutoMart/UI/img/7.jpg','truck','fusso')`,
 
      `INSERT INTO cars(id,owner, createdon, state, status, price, manufacturer, model, image, body_type, carName)
-     VALUES(6,willy@gmail.com,'12/04/2019','new','available',10000.0,'v8','land-cruiser','https://william000000.github.io/AutoMart/UI/img/7.jpg','car','land-cruiser')`,
+     VALUES(6,'willy@gmail.com','12/04/2019','new','available',10000.0,'v8','land-cruiser','https://william000000.github.io/AutoMart/UI/img/7.jpg','car','land-cruiser')`,
 
      `INSERT INTO cars(id,owner, createdon,state, status, price, manufacturer, model, image, body_type, carName)
-     VALUES(7,willy@gmail.com,'12/04/2019','used','available',20000.0,'fusso','huidai','https://william000000.github.io/AutoMart/UI/img/7.jpg','truck','huidai')`,
+     VALUES(7,'willy@gmail.com','12/04/2019','used','available',20000.0,'fusso','huidai','https://william000000.github.io/AutoMart/UI/img/7.jpg','truck','huidai')`,
 
     `insert into flags(id,car_id,createdon,reason,description) values (1,1,'12/04/2019','pricing','Has weak technology...')`,
     `insert into flags(id,car_id,createdon,reason,description) values (2,2,'12/04/2019','weirds demands','Engine not working well...')`,
 
-    `insert into orders(id,buyer,car_id,amount,status) values (1,'willy@gmail.com',1,10000,'accepted')`,
+    `insert into orders(id,buyer,car_id,amount,status) values (1,'willy@gmail.com',4,10000,'accepted')`,
     `insert into orders(id,buyer,car_id,amount,status) values (2,'willy@gmail.com',2,10000,'pending')`,
-    `insert into orders(id,buyer,car_id,amount,status) values (3,'willy@gmail.com',3,10000,'accepted')`,
 
 ];
 
@@ -118,9 +117,6 @@ const createTables = async () => {
         console.log("Dummy data inserted");
         dummy.forEach(async query=>{console.log(query);await pool.query(query);});
     }
-
-    
-    pool.end();
 
 };
 createTables();
