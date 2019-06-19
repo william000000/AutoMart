e333c45e059f6d4d5fa66538cc79f0b748dc0d8b
William000000 <sugiraw@gmail.com>
2019-06-19 10:41:08 +0200

    [#166786113 Reset password Feature] User can reset password

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -2,12 +2,17 @@ import express from "express";
 import userController from "../controllers/users";
 import auth from "../middleware/auth";
 import validateUser from "../helper/authValidation";
+import newpass from "../controllers/resetpassword";
+import Securities from "../middleware/authUser";
+
 const router = express.Router();
 
 const { validateSignup } = validateUser;
 const { validateLogin } = validateUser;
+const { isUserLogged } = Securities;
+
 router.post("/signup", validateSignup, userController.signup);
 router.post("/signin", validateLogin, userController.signin);
-
+router.post("/resetpassword",isUserLogged, newpass.reset);
 
 export default router;
